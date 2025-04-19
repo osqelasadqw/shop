1edd49a2edfb5c73f773a6d6acbe3d225e12fc6b
Your Name <your.email@example.com>
2025-04-19 19:37:33 +0400

    sadqsxzcw

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  // output: 'export', // მოვხსენით სტატიკური ექსპორტის რეჟიმი
+  output: 'export',
   basePath: '/shop',
   assetPrefix: '/shop/',
   trailingSlash: true,
